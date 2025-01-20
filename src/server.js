4179b0ce15f7d5af0fb867011584017de7d24b5d
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2025-01-20 01:23:03 +0100

    hm4

--- src/server.js
+++ src/server.js
@@ -1,22 +1,42 @@
-import express from "express";
-import cors from "cors";
-import { getEnvVar } from "./utils/getEnvVar.js";
-import contactsRouter from "./routers/contacts.js";
-import { notFoundHandler } from "./middlewares/notFoundHandler.js";
-import { errorHandler } from "./middlewares/errorHandler.js";
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import router from './routers/contacts.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
 
-export const setupServer = () => {
+export function setupServer() {
   const app = express();
 
+
   app.use(cors());
-  app.use(express.json());
-  app.use("/contacts", contactsRouter);
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('/contacts', router);
+
+  app.get('/', async (req, res) => {
+    res.status(200).json({
+      message: 'Contact list',
+    });
+  });
+
+
   app.use(notFoundHandler);
+
+
   app.use(errorHandler);
 
-  const port = Number(getEnvVar("PORT", 3000));
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
-};
+  return app;
+}
