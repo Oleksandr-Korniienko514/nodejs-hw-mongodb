85c7be6864bf752aa7c8041994571b34f0f50149
Oleksandr-Korniienko514 <bon.bon199993@gmail.com>
2025-01-24 03:38:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import { setupServer } from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import dotenv from 'dotenv';
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './contacts/index.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./contacts/index.js";
+import { initMongoConnection } from "./db/initMongoConnection.js";
+import { setupServer } from "./server.js";
+import { createDirIfNotExists } from "./utils/createDirIfNotExists.js";
 
-dotenv.config();
-
-const bootstrap = async () => {
+(async () => {
   await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
-
   setupServer();
-};
-
-bootstrap();
+})();
